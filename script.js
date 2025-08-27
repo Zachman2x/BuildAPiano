382ff8344f212fbb74f86998ef74f9178ce7dbb3
Zachman2x <60160852+Zachman2x@users.noreply.github.com>
2025-08-27 16:37:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,22 +22,3 @@ document.addEventListener('keydown', (e) => {
     return;
   }
 });
-
-
-//Starter code
-
-// const keys = document.querySelectorAll('.key');
-
-// keys.forEach((key) => {
-//   key.addEventListener('click', () => playNote(key));
-// });
-
-// function playNote(key) {
-//   const noteAudio = document.getElementById(key.dataset.note);
-//   noteAudio.currentTime = 0;
-//   noteAudio.play();
-//   key.classList.add('active'); 
-//   noteAudio.addEventListener('ended', () => {
-//     key.classList.remove('active');
-//   });
-// }
