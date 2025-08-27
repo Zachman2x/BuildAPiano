b9cddc2025090da4fa68fc9f1a968f7f06a833ce
Zachman2x <60160852+Zachman2x@users.noreply.github.com>
2025-08-27 16:41:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+const keys = document.querySelectorAll('.key');
 const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
 
 const whiteKeys = document.querySelectorAll('.key.white');
 const blackKeys = document.querySelectorAll('.key.black');
 
+keys.forEach((key) => {
+  key.addEventListener('click', () => playNote(key));
+});
+
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note);
+  noteAudio.currentTime = 0;
+  noteAudio.play();
+  key.classList.add('active'); 
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
+}
+
+// Key Trigger Event
 document.addEventListener('keydown', (e) => {
   // Get the key that triggered the event
   const key = e.key;
@@ -22,3 +38,4 @@ document.addEventListener('keydown', (e) => {
     return;
   }
 });
+
